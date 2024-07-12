556babc20f1b4ce1c11113788be737c198ebe6a7
S M A Tousi <mohamadali.tousi@gmail.com>
2024-07-12 16:27:37 -0500

    added

--- gully_detection/train_rgb_accel.py
+++ gully_detection/train_rgb_accel.py
@@ -15,7 +15,7 @@ from accelerate.utils import DistributedDataParallelKwargs
 from model import *
 from dataset import *
 from utils import *
-from loss_functions import *
+# from loss_functions import *
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 
