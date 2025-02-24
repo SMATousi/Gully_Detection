52ab75a15e88c858c2239e6786e9277448f1a333
S M A Tousi <mohamadali.tousi@gmail.com>
2025-02-24 17:04:38 -0600

    vit added

--- gully_detection/model.py
+++ gully_detection/model.py
@@ -82,8 +82,8 @@ class ViT_Gully_Classifier(nn.Module):
         
     def forward(self, list_of_images):
 
-        preprocessed_images = [self.preprocessor(images=image, return_tensors="pt")['pixel_values'] for image in list_of_images]
-        embedding_outputs = [self.embedding(preprocessed_image) for preprocessed_image in preprocessed_images]
+        # preprocessed_images = [self.preprocessor(images=image, return_tensors="pt")['pixel_values'] for image in list_of_images]
+        embedding_outputs = [self.embedding(image) for image in list_of_images]
         encoder_outputs = [self.encoder(embedding_output) for embedding_output in embedding_outputs]
         layer_norm_outputs = [self.layernorm(encoder_output[0]) for encoder_output in encoder_outputs]
         pooled_outputs = [layer_norm_output[:, 0] for layer_norm_output in layer_norm_outputs]
