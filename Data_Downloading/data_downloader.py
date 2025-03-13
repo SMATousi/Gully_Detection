083bb2d0f06bc9e3c86c4f7d9e851da02482e07d
S M A Tousi <mohamadali.tousi@gmail.com>
2025-03-13 13:51:12 -0500

    added

--- Data_Downloading/data_downloader.py
+++ Data_Downloading/data_downloader.py
@@ -9,10 +9,29 @@ from retry import retry
 import json
 import yaml
 import os
+import rasterio
 from utils import *
 
-
+#ee.Authenticate()
 ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com')
+# ee.Initialize()
+def get_region_points_utm(tif_path):
+    """Extract bounding box in original UTM coordinates."""
+    with rasterio.open(tif_path) as dataset:
+        tif_crs = dataset.crs.to_string()  # Store UTM projection
+        bounds = dataset.bounds  # (left, bottom, right, top)
+
+        # Keep the coordinates in their original UTM projection
+        region_points = {
+            "point_1": [bounds.top, bounds.left],   # Top-left (N, E)
+            "point_2": [bounds.top, bounds.right],  # Top-right (N, E)
+            "point_3": [bounds.bottom, bounds.right],  # Bottom-right (N, E)
+            "point_4": [bounds.bottom, bounds.left]   # Bottom-left (N, E)
+        }
+        return region_points, tif_crs  # Return UTM bounding box & CRS
+
+
+
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -42,44 +61,50 @@ print(config['rgb']['spec']['filter_dates'])
 
 # print(f"spec : {len(spec_config['point_1'])}")
 
-# region = ee.Geometry.Polygon(
-#     [
-#         [
-#             config['region']['point_1'][::-1],
-#             config['region']['point_2'][::-1],
-#             config['region']['point_3'][::-1],
-#             config['region']['point_4'][::-1],
-#         ]
-#     ],
-#     None,
-#     False,
-# )
+if config['specs']['from_tif']:
+    
+    tif_file_path = config['region']['tif_file_path']
+    region_points, utm_crs = get_region_points_utm(tif_file_path)
 
-region = ee.Geometry.Polygon(
-    [
+    region = ee.Geometry.Polygon(
         [
-            config['region']['point_1'],
-            config['region']['point_2'],
-            config['region']['point_3'],
-            config['region']['point_4'],
-        ]
-    ],
-    None,
-    False,
-)
+            [
+            [region_points["point_1"][1], region_points["point_1"][0]],  # (E, N)
+            [region_points["point_2"][1], region_points["point_2"][0]],
+            [region_points["point_3"][1], region_points["point_3"][0]],
+            [region_points["point_4"][1], region_points["point_4"][0]]
+            ]
+        ],
+        None,
+        False,
+    )
 
-# region = ee.Geometry.Polygon(
-#     [
-#         [
-#             bounding_box_coordinates[0],
-#             bounding_box_coordinates[1],
-#             bounding_box_coordinates[2],
-#             bounding_box_coordinates[3],
-#         ]
-#     ],
-#     None,
-#     False,
-# )
+else:
+    region = ee.Geometry.Polygon(
+        [
+            [
+                config['region']['point_1'],
+                config['region']['point_2'],
+                config['region']['point_3'],
+                config['region']['point_4'],
+            ]
+        ],
+        None,
+        False,
+    )
+
+    # region = ee.Geometry.Polygon(
+    #     [
+    #         [
+    #             bounding_box_coordinates[0],
+    #             bounding_box_coordinates[1],
+    #             bounding_box_coordinates[2],
+    #             bounding_box_coordinates[3],
+    #         ]
+    #     ],
+    #     None,
+    #     False,
+    # )
 
 if config['rgb']['choose']:
 
@@ -216,8 +241,9 @@ if config['dem']['choose']:
         url = image.getDownloadURL(
             {
                 'region': region,
-                'dimensions': params['dimensions'],
+                # 'dimensions': params['dimensions'],
                 'format': params['format'],
+                'scale': config['dem']['spec']['scale']
             }
         )
         
