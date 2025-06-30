08a022336ab1d4614fc8cbf10a8007c9434a6edd
SMATousi <mohamadali.tousi@gmail.com>
2025-06-30 16:37:30 -0500

    gully ws datasets classes added

--- gully_detection/dataset.py
+++ gully_detection/dataset.py
@@ -140,6 +140,7 @@ class SixImageDataset_DEM_GT(Dataset):
 
 
 
+
 class SixImageDataset_DEM_GT_Geo(Dataset):
     def __init__(self, pos_dir, 
                  neg_dir, 
@@ -515,3 +516,185 @@ class EightImageDataset_DEM_GT_Geo_from_JSON(Dataset):
         label = torch.tensor(self.labels[idx], dtype=torch.float32)
         geo_info = self.geo_info[idx]
         return images, dem_image, gt_mask, label
+
+
+
+
+
+class EightImageDataset_WS(Dataset):
+    def __init__(self, image_dir,
+                 label_model_results_path,
+                 transform=None,
+                 oversample=False):
+
+        self.image_dir = image_dir
+        self.transform = transform
+        self.oversample = oversample
+        self.label_model_results_path = label_model_results_path
+        
+        self.data = []
+        self.labels = []
+        self.geo_info = []
+
+        self.label_model_results = self.load_label_model_results()
+        
+        # Load all files and group by tiles
+        files = [f for f in os.listdir(image_dir) if f.endswith('.tif')]
+        tiles = self.group_files_by_tile(files)
+
+        # Handle class imbalance by oversampling the minority class
+        if self.oversample:
+            max_len = max(len(tiles))
+            tiles = self.oversample(tiles, max_len)
+
+        # Combine and store
+        self.store_tiles(tiles, image_dir, 1)
+        
+        self.resize_high = transforms.Resize((764,764))
+
+    def load_label_model_results(self):
+        return json.load(open(self.label_model_results_path, 'r'))
+
+    def group_files_by_tile(self, files):
+        tile_dict = {}
+        for file in files:
+            tile_number = file.split('_')[-1].split('.')[0]
+            if tile_number not in tile_dict:
+                tile_dict[tile_number] = []
+            tile_dict[tile_number].append(file)
+        # Only include complete groups
+        return [tile for tile in tile_dict.values() if len(tile) == 8]
+
+    def oversample(self, tiles, target_length):
+        # Repeat tiles until the desired length is achieved
+        return random.choices(tiles, k=target_length)
+
+    def store_tiles(self, tiles, directory, label):
+        for tile_files in tiles:
+            tile_number = tile_files[0].split('_')[-1].split('.')[0]
+            self.data.append([os.path.join(directory, f) for f in sorted(tile_files)])
+            self.labels.append(label)
+            self.geo_info.append(self.extract_geo_info(os.path.join(directory, tile_files[0])))
+
+    def extract_geo_info(self, file_path):
+        with rasterio.open(file_path) as dataset:
+            geo_transform = dataset.transform
+            crs = dataset.crs
+        return {'geo_transform': geo_transform, 'crs': crs}
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        images_1 = [imageio.imread(img_path).astype('uint8') for img_path in self.data[idx]]
+        images = [transforms.functional.to_pil_image(image).convert("RGB") for image in images_1]
+        if self.transform:
+            # Generate a random seed for this tile
+            seed = torch.randint(0, 2**32, (1,)).item()
+            transformed_images = []
+            for i, image in enumerate(images):
+                torch.manual_seed(seed)
+                random.seed(seed)
+                if i == 1:
+                    transformed_images.append(self.transform(self.resize_high(image)))
+                else:
+                    transformed_images.append(self.transform(image))
+            images = transformed_images
+            torch.manual_seed(seed)
+            random.seed(seed)
+        
+        target_probs = torch.tensor(self.label_model_results[idx]['proba'], dtype=torch.float32)
+        target_label = torch.tensor(int(self.label_model_results[idx]['label']), dtype=torch.float32)
+
+        # geo_info = self.geo_info[idx]
+
+        return images, target_probs, target_label
+
+
+class EightImageDataset_WS_GT(Dataset):
+    def __init__(self, image_dir,
+                 GT_labels_path,
+                 transform=None,
+                 oversample=False):
+
+        self.image_dir = image_dir
+        self.transform = transform
+        self.oversample = oversample
+        self.GT_labels_path = GT_labels_path
+        
+        self.data = []
+        self.labels = []
+        self.geo_info = []
+
+        self.GT_labels = self.load_GT_labels()
+        
+        # Load all files and group by tiles
+        files = [f for f in os.listdir(image_dir) if f.endswith('.tif')]
+        tiles = self.group_files_by_tile(files)
+
+        # Handle class imbalance by oversampling the minority class
+        if self.oversample:
+            max_len = max(len(tiles))
+            tiles = self.oversample(tiles, max_len)
+
+        # Combine and store
+        self.store_tiles(tiles, image_dir, 1)
+        
+        self.resize_high = transforms.Resize((764,764))
+
+    def load_GT_labels(self):
+        return json.load(open(self.GT_labels_path, 'r'))
+
+    def group_files_by_tile(self, files):
+        tile_dict = {}
+        for file in files:
+            tile_number = file.split('_')[-1].split('.')[0]
+            if tile_number not in tile_dict:
+                tile_dict[tile_number] = []
+            tile_dict[tile_number].append(file)
+        # Only include complete groups
+        return [tile for tile in tile_dict.values() if len(tile) == 8]
+
+    def oversample(self, tiles, target_length):
+        # Repeat tiles until the desired length is achieved
+        return random.choices(tiles, k=target_length)
+
+    def store_tiles(self, tiles, directory, label):
+        for tile_files in tiles:
+            tile_number = tile_files[0].split('_')[-1].split('.')[0]
+            self.data.append([os.path.join(directory, f) for f in sorted(tile_files)])
+            self.labels.append(label)
+            self.geo_info.append(self.extract_geo_info(os.path.join(directory, tile_files[0])))
+
+    def extract_geo_info(self, file_path):
+        with rasterio.open(file_path) as dataset:
+            geo_transform = dataset.transform
+            crs = dataset.crs
+        return {'geo_transform': geo_transform, 'crs': crs}
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        images_1 = [imageio.imread(img_path).astype('uint8') for img_path in self.data[idx]]
+        images = [transforms.functional.to_pil_image(image).convert("RGB") for image in images_1]
+        if self.transform:
+            # Generate a random seed for this tile
+            seed = torch.randint(0, 2**32, (1,)).item()
+            transformed_images = []
+            for i, image in enumerate(images):
+                torch.manual_seed(seed)
+                random.seed(seed)
+                if i == 1:
+                    transformed_images.append(self.transform(self.resize_high(image)))
+                else:
+                    transformed_images.append(self.transform(image))
+            images = transformed_images
+            torch.manual_seed(seed)
+            random.seed(seed)
+        
+        target_label = torch.tensor(int(self.GT_labels[idx]), dtype=torch.float32)
+
+        # geo_info = self.geo_info[idx]
+
+        return images, target_label
